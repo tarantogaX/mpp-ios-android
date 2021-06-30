9089c8e60b8ed065c4379e264ca8846da44d2ea3
alekow <ijontichy1024@gmail.com>
2021-06-30 10:16:20 +0100

    Part 1

--- app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.jetbrains.handson.mpp.mobile
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Spinner
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), ApplicationContract.View {
+
+    lateinit var departureStationSpinner: Spinner
+    lateinit var arrivalStationSpinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,9 +19,34 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
 
         val presenter = ApplicationPresenter()
         presenter.onViewTaken(this)
+
+        departureStationSpinner = findViewById<Spinner>(R.id.stations_spinner1) as Spinner
+        arrivalStationSpinner = findViewById<Spinner>(R.id.stations_spinner2) as Spinner
+        departureStationSpinner.setSelection(0)
+        arrivalStationSpinner.setSelection(1)
     }
 
     override fun setLabel(text: String) {
-        findViewById<TextView>(R.id.main_text).text = text
+        //findViewById<TextView>(R.id.main_text).text = text
     }
+
+    fun onSubmitClicked(view: View) {
+
+        var departureCode = departureStationSpinner.selectedItem.toString()
+        val ld = departureCode.length
+        departureCode = departureCode.substring(ld - 4, ld - 1)
+
+        var arrivalCode = arrivalStationSpinner.selectedItem.toString()
+        val ad = arrivalCode.length
+        arrivalCode = arrivalCode.substring(ad - 4, ad - 1)
+        val url =
+            "https://www.lner.co.uk/travel-information/travelling-now/live-train-times/depart/$departureCode/$arrivalCode/#LiveDepResults"
+
+
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        startActivity(intent)
+    }
+
+    //https://www.lner.co.uk/travel-information/travelling-now/live-train-times/depart/YRK/KGX/#LiveDepResults
 }
--- native/KotlinIOS/KotlinIOS/ViewController.swift
+++ native/KotlinIOS/KotlinIOS/ViewController.swift
@@ -1,20 +1,52 @@
 import UIKit
 import SharedCode
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet private var label: UILabel!
-
+    @IBOutlet private var pickerViewDeparture: UIPickerView!
+    @IBOutlet private var pickerViewArrival: UIPickerView!
+    @IBOutlet private var submitButton: UIButton!
+    
     private let presenter: ApplicationContractPresenter = ApplicationPresenter()
+    
+    let stations = ["Colchester [COL]","Ipswich [IPS]","Birmingham New Street [BHM]","Kings Cross [KGX]", "Cambridge [CBG]"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+        
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stations.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return stations[row]
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let arr = pickerViewArrival.selectedRow(inComponent: 0)
+        let dep = pickerViewDeparture.selectedRow(inComponent: 0)
+        let departure = stations[dep].suffix(4).prefix(3)
+        let arrival = stations[arr].suffix(4).prefix(3)
+        if let url = URL(string: "https://www.lner.co.uk/travel-information/travelling-now/live-train-times/depart/" + departure + "/" + arrival +  "/#LiveDepResults") {
+            UIApplication.shared.open(url)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter.onViewTaken(view: self)
+        
+        self.pickerViewDeparture.delegate = self
+        self.pickerViewDeparture.dataSource = self
+        
+        self.pickerViewArrival.delegate = self
+        self.pickerViewArrival.dataSource = self
     }
 }
 
 extension ViewController: ApplicationContractView {
     func setLabel(text: String) {
-        label.text = text
     }
 }
